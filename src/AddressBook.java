6fcd0a455f6c5b46cdce2f1774abf2cb45ff5298
Quinton <quintontracey01@gmail.com>
2021-09-17 12:22:41 -0400

    Added main method which prints a message.
    Created an AddressBook, added then removed a created buddy.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,10 +5,15 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy1 = new BuddyInfo("Pablo", "Carleton", "4166666666");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy1);
+        addressBook.removeBuddy(buddy1);
+
     }
 
-    public AddressBook(ArrayList<BuddyInfo> buddies) {
-        this.buddies = buddies;
+    public AddressBook() {
+        this.buddies = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo buddy){
